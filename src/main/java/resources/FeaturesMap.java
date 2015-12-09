eeac800f70bd367ef6c837960486fca6a4e38ce9
adrien Bodineau <adrien.bodineau@gmail.com>
2015-12-09 08:43:57 +0100

    Create FeaturesMap.java

---
+++ src/main/java/resources/FeaturesMap.java
@@ -0,0 +1,26 @@
+package resources;
+
+public class FeaturesMap {
+	// Utilitarian class used to store features needed for the construction of the WEKA model
+
+	public int nb_of_features;
+	public int tf;
+	public int df;
+	public double idf;
+	public double tfidf;
+	public int firstOccurence;
+	public int lastOccurence;
+	public int spread;
+	
+	public FeaturesMap(int tf, int df, double idf, double tfidf, int firstOccurence, int lastOccurence, int spread) {
+		this.nb_of_features = 7;
+		this.tf = tf;
+		this.df = df;
+		this.idf = idf;
+		this.tfidf = tfidf;
+		this.firstOccurence = firstOccurence;
+		this.lastOccurence = lastOccurence;
+		this.spread = spread;
+	}
+}
+
