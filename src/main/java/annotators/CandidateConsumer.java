a566b353acda8b3ad05171c659de00c78ea2f080
codeOverFLow <adrien.bodineau@gmail.com>
2015-12-17 15:20:36 +0100

    add filename to the resource

--- src/main/java/annotators/CandidateConsumer.java
+++ src/main/java/annotators/CandidateConsumer.java
@@ -1,15 +1,21 @@
 package annotators;
 
+import static org.apache.uima.fit.util.JCasUtil.select;
+
+import java.util.ArrayList;
 import java.util.TreeMap;
 
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.ExternalResource;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import resources.Candidate;
 import resources.CandidateList;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 
 public class CandidateConsumer extends JCasAnnotator_ImplBase {
   public final static String CANDIDATE_KEY = "candidateKey";
@@ -21,6 +27,8 @@ public class CandidateConsumer extends JCasAnnotator_ImplBase {
 
   @ConfigurationParameter(name = PARAM_FILENAME, mandatory = true, defaultValue = "target/candidates.txt")
   private String filename_;
+  
+  private ArrayList<String> files_;
 
   @Override
   public void collectionProcessComplete() throws AnalysisEngineProcessException {
@@ -41,12 +49,21 @@ public class CandidateConsumer extends JCasAnnotator_ImplBase {
                 / (collection.getAllCandidates().size() + 1.0)));
       }
     }
-    collection.save(filename_);
+    collection.save(filename_, files_);
     System.out.println("Candidates successfully saved to " + filename_);
   }
 
   @Override
-  public void process(JCas arg0) throws AnalysisEngineProcessException {
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
+    for (DocumentMetaData dmd : select(jcas, DocumentMetaData.class)) {
+      files_.add(dmd.getDocumentTitle());
+    }
+  }
 
+  @Override
+  public void initialize(UimaContext context) throws ResourceInitializationException {
+    // TODO Auto-generated method stub
+    super.initialize(context);
+    files_ = new ArrayList<String>();
   }
 }
